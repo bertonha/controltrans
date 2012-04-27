4913e79865ef08a0b7893fa4f15da644ed79fa6f
Christofer Bertonha <christoferbertonha@gmail.com>
2012-04-26 23:15:11 -0300

    Better abstration from model

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from core.models import Caminhao, Cliente, Fornecedor, Transportadora
+from core.models import Caminhao, Cliente, Fornecedor, Nota, Transportadora
 
 admin.site.register(Caminhao)
 admin.site.register(Cliente)
 admin.site.register(Fornecedor)
+admin.site.register(Nota)
 admin.site.register(Transportadora)
--- core/models.py
+++ core/models.py
@@ -3,28 +3,28 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 
-class Endereco(models.Model):
+class CommonFields(models.Model):
     rua = models.CharField(max_length=128)
     numero = models.PositiveIntegerField(_(u'Número'))
     bairro = models.CharField(max_length=128)
     cidade = models.CharField(max_length=64)
     estado = models.CharField(max_length=2)
     cep = models.CharField(_('CEP'), max_length=9)
-
-    class Meta:
-        abstract = True
-
-
-class Transportadora(Endereco):
-    cnpj = models.CharField(max_length=14)
-    ie = models.CharField(max_length=14)
-    razao_social = models.CharField(max_length=256)
-    logo = models.ImageField(upload_to='transportadora')
+    cnpj = models.CharField(_('CNPJ'), max_length=14)
+    ie = models.CharField(_('IE'), max_length=14)
+    razao_social = models.CharField(_(u'Razão Social'), max_length=256)
     telefone = models.CharField(max_length=11)
     email = models.EmailField(unique=True)
 
     def __unicode__(self):
         return self.razao_social
+
+    class Meta:
+        abstract = True
+
+
+class Transportadora(CommonFields):
+    logo = models.ImageField(upload_to='transportadora')
 
 
 class Caminhao(models.Model):
@@ -52,23 +52,12 @@ class Caminhao(models.Model):
         return self.placa
 
 
-class Fornecedor(Endereco):
-    cnpj = models.CharField(_('CNPJ'), max_length=14)
-    ie = models.CharField(_('IE'), max_length=14)
-    razao_social = models.CharField(_(u'Razão Social'), max_length=256)
-    telefone = models.CharField(max_length=11)
-    email = models.EmailField(unique=True)
-
-    def __unicode__(self):
-        return self.razao_social
+class Fornecedor(CommonFields):
+    pass
 
 
-class Cliente(Endereco):
-    cnpj = models.CharField(max_length=14)
-    ie = models.CharField(max_length=14)
-    razao_social = models.CharField(max_length=256)
-    telefone = models.CharField(max_length=11)
-    email = models.EmailField(unique=True)
+class Cliente(CommonFields):
+    pass
 
 
 class Nota(models.Model):
@@ -79,4 +68,4 @@ class Nota(models.Model):
     #seguro
     #itens
     natureza_mercadoria = models.CharField(max_length=16, default='Etanol')
-    dt_emissao = models.DateTimeField()
+    dt_emissao = models.DateTimeField(_(u'Data Emissão'))
